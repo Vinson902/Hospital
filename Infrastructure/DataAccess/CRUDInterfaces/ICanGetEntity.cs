4a1f396cbba629db35d3efb06f0b65f3163ceab4
Rheper Vinson <rheper.vinson@yandex.ru>
2020-09-12 19:47:46 +0100

    Created a new project "Infrastructure" with CRUD interfaces

---
+++ Infrastructure/DataAccess/CRUDInterfaces/ICanGetEntity.cs
@@ -0,0 +1,12 @@
+﻿using Hospital.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.CRUDInterfaces
+{
+    interface ICanGetEntity<TEntity> where TEntity : Entity
+    {
+        void Get(TEntity entity);
+    }
+}
